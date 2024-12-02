bdda55e98a7ca7ecea85ae42e0a49c267667eb94
Furkan Enes Genç <beyazenes@hotmail.com>
2024-12-02 20:37:27 +0300

    First commit . Started spring boot version with 3.4.0. Necessary dependencies were added.

---
+++ credit/src/test/java/loan/api/credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package loan.api.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
